717197fca2bf50e1e9d03c23e5c14a31fdb1dc30
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2017-10-14 03:17:32 -0400

    no real reason

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/users_test");
+mongoose.connect("mongodb://localhost:users_test/users_test");
 mongoose.connection
   .once("open", () => console.log("DB Running =]"))
   .on("error", (error) => {
