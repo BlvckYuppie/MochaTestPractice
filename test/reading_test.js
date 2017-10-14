db13b5f8ad79859209dceb50c5d63700889e4b50
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2017-10-14 05:06:05 -0400

    done for the night, headed home... Tommorow going to try to write tests myself.

--- test/reading_test.js
+++ test/reading_test.js
@@ -3,23 +3,46 @@ const User = require("../src/user");
 
 describe("Searching For User Record",() => {
   let joe;
+  let moe;
+  let hoe;
 
   beforeEach((done) => {
     joe = new User({
       name: "Joe"
     });
+    moe = new User({
+      name: "Moe"
+    });
+    hoe = new User({
+      name: "Hoe"
+    });
 
     joe.save()
       .then(() => {
-        done();
+        moe.save()
+          .then(() => {
+            hoe.save()
+              .then(() => {
+                done();
+              })
+          })
       });
   });
 
   it('Finds all users in the db named joe',(done) => {
     User.find({name:'Joe'})
       .then((users) => {
-        assert(users[0]._id.toString() === joe._id.toString());
+        assert(users[0]._id.toString() == joe._id.toString());
         done();
       });
   });
+
+  it("Finds A User by Id",(done) => {
+    User.findOne({_id : joe._id})
+      .then((users) => {
+        assert(users.name === joe.name);
+        done();
+      });
+
+  })
 });
