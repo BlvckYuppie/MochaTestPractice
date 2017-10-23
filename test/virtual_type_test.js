d01679b2ca0dc61934a61a0aa275ab356e18823a
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2017-10-23 15:59:51 -0400

    began working on 'virtual type' test for post count

---
+++ test/virtual_type_test.js
@@ -0,0 +1,21 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe('Virtual Types', () => {
+
+  it('postcount returns number of posts', (done) => {
+    cosnt joe = new User({
+      name:"Joe",
+      posts: [{title:"1st post"}]
+    });
+
+    joe.save()
+      .then(() => User.findOne({name:"Joe"}))
+      .then((user)=> {
+
+        assert(joe.postCount === 1);
+        done();
+      })
+  });
+
+});
