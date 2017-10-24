634c8b4d03c2d1daa8fa2635a7126f9e49a4cdf2
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2017-10-24 04:32:49 -0400

    added middleware and a test 4 it, my hdd is slow, so I added additional time till timeout in package.json/ test / mocha

---
+++ test/middleware_test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const User = require('../src/user');
+const BlogPost = require('../src/blogPost');
+const assert = require('assert');
+
+describe('Middleware', () => {
+  let joe, blogPost;
+
+  beforeEach((done) => {
+
+    joe = new User({name:"joe"});
+    blogPost = new BlogPost({
+      title:"new post",
+      content:"what the title said",
+    });
+
+    joe.blogPosts.push(blogPost);
+
+    Promise.all([joe.save(), blogPost.save()])
+    .then(() => done());
+  });
+
+  it.only('Test to see if blogposts are destroyed with users Removal', (done) => {
+      joe.remove()
+      .then(() => BlogPost.count())
+      .then((count) => {
+        console.log(count);
+        assert (count === 0);
+        done();
+    })
+  });
+
+});
