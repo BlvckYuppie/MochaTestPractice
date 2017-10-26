3d60d6a62c9f6d79dac14c780cc2ee84976c6e48
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2017-10-26 04:27:37 -0400

    added skip and limit test to 'read' test suite

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,24 +2,27 @@ const assert = require ("assert");
 const User = require("../src/user");
 
 describe("Searching For User Record",() => {
-  let joe;
-  let moe;
-  let hoe;
+
+  let joe, moe, hoe, foe;
 
   beforeEach((done) => {
+
+    foe = new User({
+      name: "Foe"
+    });
+    hoe = new User({
+      name: "Hoe"
+    });
     joe = new User({
       name: "Joe"
     });
     moe = new User({
       name: "Moe"
     });
-    hoe = new User({
-      name: "Hoe"
-    });
 
-    Promise.All([ joe.save(), moe.save(), hoe.save()])
-      .then(() => done());    
-  )};
+    Promise.all([foe.save(),moe.save(), joe.save(),hoe.save(),  ])
+      .then(() => done());
+  });
 
   it('Finds all users in the db named joe',(done) => {
     User.find({name:'Joe'})
@@ -35,6 +38,19 @@ describe("Searching For User Record",() => {
       assert(users.name === joe.name);
       done();
     });
+  });
 
-  })
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+    .sort({name: 1})
+    .skip(1)
+    .limit(2)
+      .then((users)=>{
+        assert(users.length === 2);
+        assert(users[0.].name === "Hoe");
+        assert(users[1].name === "Joe");
+        done();
+      });
+  });
+
 });
